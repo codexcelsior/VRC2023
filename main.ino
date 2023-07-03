e6156c828bfcb0eb225a1d77f52dd351a7426ed6
Quang Nguyen <quang13012005@gmail.com>
2023-07-03 23:51:40 +0700

    Adding servo ports and shooter function

--- main.ino
+++ main.ino
@@ -1,6 +1,6 @@
-//Import headers của các thư viện 
-#include <PS2X_lib.h>  
-#include <Adafruit_PWMServoDriver.h> 
+//Import headers của các thư viện
+#include <PS2X_lib.h>
+#include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
 
 //Set up các chân tín hiệu của tay cầm PS2
@@ -8,7 +8,6 @@
 #define PS2_CMD 13
 #define PS2_CLK 14
 #define PS2_ATT 15
-
 
 
 // Kênh PWM (Chân của các servo và DC motor) điều khiển động cơ (Số chân của động cơ DC hay servo đều gấp đôi số kênh của động cơ đó) (Nếu vẫn sai chân thì invert lại)
@@ -24,19 +23,23 @@
 #define PWM_DC3A 10
 #define PWM_DC3B 11
 
+//Kenh PWM cho servo
+#define PWM_SERVO0          2
+#define PWM_SERVO1          3
+#define PWM_SERVO2          4
+#define PWM_SERVO3          5
+#define PWM_SERVO4          6
+#define PWM_SERVO5          7
+
 // Động cơ DC
 #define MOT_LEFT 1
 #define MOT_RIGHT 2
-#define SHOOTER_1 3
+#define MOT_SHOOTER 3
 
 // set tốc độ động cơ
 #define SPD_FAST 2047
+#define SPD_MAX  4095
 
-#define PWM_SERVO0          8
-#define PWM_SERVO1          9
-#define PWM_SERVO2          10
-#define PWM_SERVO3          11
-#define PWM_SERVO4          12
 
 // Độ rộng xung cho servo 360 (đã calibrate)
 #define SRV_360_T1          698 // độ rộng xung mà tại đó servo quay nhanh nhất theo 1 chiều
@@ -45,12 +48,13 @@
 #define SRV_360_T4          2104 // độ rộng xung mà tại đó servo quay nhanh nhất theo chiều còn lại
 
 // Cổng của servo
-#define SRV_INTAKE          0
-#define SRV_WHEEL           1
+#define SRV_INTAKE          7
+#define SRV_WHEEL           6
 
 // Tốc độ động cơ
 #define SPD_INTAKE          100 // servo
 #define SPD_WHEEL           90
+
 
 
 //Khai báo linh kiện
@@ -60,23 +64,19 @@ PS2X ps2;
 void setup() {  //Hàm set up chạy khởi tạo một lần khi khởi động mạch
   Serial.begin(115200); //Bật serial monitor ở tốc độ 115200
 
-  Serial.println("Welcome to Mastodon's BanhMi. Starting Serial Monitor...");
+  Serial.println("Mastodon's BanhMi Connected To PC. Proceeding to connect PS2 Controller");
 
   while (ps2.config_gamepad(PS2_CLK, PS2_CMD, PS2_ATT, PS2_DAT) != 0) {
-    Serial.println("Error: PS2 Controller Not Connected");
+    Serial.println("Err: PS2 Controller Not Connected");
     delay(500);
   } //Check xem có chân tín hiệu nào của điều khiển nhận đc hay ko
 
-  Serial.println("Controller Connected Successfully!");
+  Serial.println("Controller works normally");
 
   pwm.begin();                           //Khởi tạo xung pwm
   pwm.setOscillatorFrequency(27000000);  //Đặt tần số xung trong PCA9685 là 27000000 (27MHz) (27 triệu)
   pwm.setPWMFreq(50);                    //Đặt tần số giao động trên chân tối đa 50Hz (Để dùng cho cả Servo) (Pulse Width Modulation)
 }
-  bool intake_toggle = false;
-  bool wheel_toggle = false;
-  bool intake = false;
-  bool wheel = false;
 
 void ctrl_dc(uint8_t motor, int16_t speed) {
   switch (motor) {
@@ -98,6 +98,7 @@ void ctrl_dc(uint8_t motor, int16_t speed) {
       break;
   }
 }
+
 
 // Hàm điều khiển vị trí servo 180 (nhận số động cơ servo từ 0->4 ứng với kênh PWM 8->12, giá trị góc từ 0 đến 180)
 void ctrl_servo180(uint8_t motor, float angle) {
@@ -127,28 +128,47 @@ void ctrl_servo360(uint8_t motor, float speed) {
   }
 }
 
-
 void loop() {
     // put your main code here, to run repeatedly: 
     
     ps2.read_gamepad();  //Khởi tạo đọc từ điều kiển bằng hàm read_gamepad()
 
+    //Define Motor:
     ctrl_dc(MOT_LEFT, map(ps2.Analog(PSS_LY), 0, 255, -SPD_FAST, SPD_FAST));
     ctrl_dc(MOT_RIGHT, map(ps2.Analog(PSS_RY), 0, 255, SPD_FAST, -SPD_FAST));
 
+
+    
+    //Toggle conditions
+    bool intake_toggle = false;
+    bool wheel_toggle = false;
+    bool shooter_toggle = false;
+    bool intake = false;
+    bool wheel = false; 
+    bool shooter = false;
+
+    
+
     //Hàm mẫu để sử dụng Servo 
     if (ps2.Button(PSB_R1)){
-      if(!intake_toggled) {
-        intake_toggled = true;
-        intake = !intake;
-        ctrl_servo360(SRV_INTAKE, (intake) ?  SPD_INTAKE : 0);
-    } else intake_toggle = false;
+        if(!intake_toggle) {  
+          intake_toggle = true;
+          intake = !intake;
+          ctrl_servo360(SRV_INTAKE, (intake) ?  SPD_INTAKE : 0);
+      } else {intake_toggle = false;}}
 
     if (ps2.Button(PSB_R2)){
-      if(wheel_toggle) = true;
-        wheel = !wheel;
-        ctrl_servo360(SRV_WHEEL, (wheel) ? SPD_WHEEL : 0);
-    } else wheel_toggle = false;
-     
-}
+        if(!wheel_toggle) {
+          wheel_toggle = true;
+          wheel = !wheel;
+          ctrl_servo360(SRV_WHEEL, (wheel) ?  SPD_WHEEL : 0);
+      } else {wheel_toggle = false;}}
+
+   if (ps2.Button(PSB_GREEN)) {
+         if(!shooter_toggle) {
+          shooter_toggle = true;
+          shooter = !shooter;
+          ctrl_dc(MOT_SHOOTER, (shooter) ? SPD_MAX : 0);
+         } else {shooter_toggle = false;}}
+         
 }
